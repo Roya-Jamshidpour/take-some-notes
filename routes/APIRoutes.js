424b0a26167cf0a3b097e2813f9cec5839ea8eed
Roya-Jamshidpour <roya.jamshidpour@gmail.com>
2022-06-20 18:16:44 -0500

    moved code to different folders

--- routes/APIRoutes.js
+++ routes/APIRoutes.js
@@ -26,37 +26,4 @@ router.delete('/notes/:id', (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
-
 module.exports = router;
-// router.get('/notes', (req, res) => {
-//     fs.readFile("./db/db.json", (err, data) => {
-//         if (err) throw err;
-//         res.json(JSON.parse(data))
-//     })
-// })
-
-// // posts new note to database
-// router.post('api/notes', (req, res) => {
-
-//     const { title, text } = req.body;
-//     // if all fields enetered then new note is saved
-//     if (title && text) {
-
-//         const newNote = {
-//             title,
-//             text,
-//             id: uuid(),
-
-//         };
-//     }
-//         const parseData = JSON.parse(newNote);
-//         console.log(JSON.parse(data));
-//         parseData.push(newNote);
-        
-//         fs.writeFile("./db/db.json", JSON.stringify(parseData), err => {
-//             if (err) throw err;
-//             res.json(parseData)
-
-//         })
-//     });
-
