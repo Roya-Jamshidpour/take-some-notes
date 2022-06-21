be9bb73b908b10aa6136900ac6f3a3f6f970b4d7
Roya-Jamshidpour <roya.jamshidpour@gmail.com>
2022-06-20 20:52:21 -0500

    cannot get notes to save

--- db/store.js
+++ db/store.js
@@ -1,42 +1,41 @@
+const router = require('express').Router();
 
-// Handle when a user submits feedback
+ // Handle when a user submits feedback
 
   // Post a new tip to the page
    const getNotes = () =>
   fetch('api/notes', {
     method: 'GET', // or 'PUT'
-    headers: {
+    newNote: {
       'Content-Type': 'application/json',
     },
-    // body: JSON.stringify(data),
+    body: JSON.stringify(data),
   })
     .then((response) => response.json())
     .then((data) => data)
-    .catch((error) => {
+    console.log("i worked")
+     if ((error) => {
       console.error('Error:', error);
     });
 
- const addNote = (notes) =>
+ const addNote = (data) =>
 fetch('api/notes', {
   method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(notes),
+  body: JSON.stringify(data),
 })
   .then((response) => response.json())
   .then((data) => {
     alert(data);
-    createCard(notes);
+    console.log('i worked again')
   })
-  .catch((error) => {
+   if ((error) => {
     console.error('Error:', error);
   });
 
-// When the page loads, get all the tips
-getNotes().then((data) => data.forEach((notes) => console.log('worked')));
+// // When the page loads, get all the tips
+// getNotes().then((data) => data.forEach((data) => console.log('worked')));
 
 
-
+  module.exports = router
   module.exports = getNotes
   module.exports = addNote
